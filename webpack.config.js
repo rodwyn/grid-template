a5ae315f5bf833a735a4655044fb0b0238fd117d
Rodwyn Moreno <rodwynmoreno@MacBook-Pro-de-Rodwyn.local>
2018-08-12 22:16:29 -0500

    Added app architecture.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: './app/scripts/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname + '/app'
+  }
+};
